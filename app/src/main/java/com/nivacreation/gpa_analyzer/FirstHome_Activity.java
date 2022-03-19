b33d1d93339e2a5d868a9991dac837cc5295e4a9
Nivesh Wanninayaka <niveshncwanninayaka@gmail.com>
2022-03-20 04:33:34 +0530

    Success grade pop dialog 2

--- app/src/main/java/com/nivacreation/gpa_analyzer/FirstHome_Activity.java
+++ app/src/main/java/com/nivacreation/gpa_analyzer/FirstHome_Activity.java
@@ -1,5 +1,6 @@
 package com.nivacreation.gpa_analyzer;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -13,7 +14,10 @@ import android.widget.Toast;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -98,12 +102,26 @@ public class FirstHome_Activity extends AppCompatActivity {
                         cminvalue = bottomSheetDialog.findViewById(R.id.CMinValue),
                         dplusvalue = bottomSheetDialog.findViewById(R.id.DPlusValue);
 
-                String apv = aplusvalue.getText().toString(), av = avalue.getText().toString(), amv = aminvalue.getText().toString(),
-                        bpv = bplusvalue.getText().toString(), bv = bvalue.getText().toString(), bmv = bminvalue.getText().toString(),
-                        cpv = cplusvalue.getText().toString(), cv = cvalue.getText().toString(), cmv = cminvalue.getText().toString(),
-                        dpv = dplusvalue.getText().toString();
+               documentReference = fStore.collection("GradesValue").document(userId);
+                documentReference.addSnapshotListener(FirstHome_Activity.this, new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable @org.jetbrains.annotations.Nullable DocumentSnapshot value, @Nullable @org.jetbrains.annotations.Nullable FirebaseFirestoreException error) {
 
+                        if (value != null && value.exists()) {
+                            aplusvalue.setText(value.getString("APV"));
+                            avalue.setText(value.getString("AV"));
+                            aminvalue.setText(value.getString("AMV"));
+                            bplusvalue.setText(value.getString("BPV"));
+                            bvalue.setText(value.getString("BV"));
+                            bminvalue.setText(value.getString("BMV"));
+                            cplusvalue.setText(value.getString("CPV"));
+                            cvalue.setText(value.getString("CV"));
+                            cminvalue.setText(value.getString("CMV"));
+                            dplusvalue.setText(value.getString("DPV"));
 
+                        }
+                    }
+                });
 
 
                 Button okBtn = bottomSheetDialog.findViewById(R.id.okBtn);
@@ -111,6 +129,7 @@ public class FirstHome_Activity extends AppCompatActivity {
                 okBtn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+
                         documentReference = fStore.collection("GradesValue").document(userId);
                         Map<String,Object> grade = new HashMap<>();
                         EditText aplusvalue = bottomSheetDialog.findViewById(R.id.APlusValue),
@@ -129,17 +148,77 @@ public class FirstHome_Activity extends AppCompatActivity {
                                 cpv = cplusvalue.getText().toString(), cv = cvalue.getText().toString(), cmv = cminvalue.getText().toString(),
                                 dpv = dplusvalue.getText().toString();
 
-                        grade.put("APV",apv);
-                        grade.put("AV",av);
-                        grade.put("AMV",amv);
-                        grade.put("BPV",bpv);
-                        grade.put("BV",bv);
-                        grade.put("BMV",bmv);
-                        grade.put("CPV",cpv);
-                        grade.put("CV",cv);
-                        grade.put("CMV",cmv);
-                        grade.put("DPV",dpv);
-                        documentReference.set(grade);
+                        if (!apv.isEmpty()){
+
+                            if (!av.isEmpty()){
+
+                                if (!amv.isEmpty()){
+
+                                    if (!bpv.isEmpty()){
+
+                                        if (!bv.isEmpty()){
+
+                                            if (!bmv.isEmpty()){
+
+                                                if (!cpv.isEmpty()){
+
+                                                    if (!cv.isEmpty()){
+
+                                                        if (!cmv.isEmpty()){
+
+                                                            if (!dpv.isEmpty()){
+
+                                                                grade.put("APV",apv);
+                                                                grade.put("AV",av);
+                                                                grade.put("AMV",amv);
+                                                                grade.put("BPV",bpv);
+                                                                grade.put("BV",bv);
+                                                                grade.put("BMV",bmv);
+                                                                grade.put("CPV",cpv);
+                                                                grade.put("CV",cv);
+                                                                grade.put("CMV",cmv);
+                                                                grade.put("DPV",dpv);
+                                                                documentReference.set(grade);
+
+                                                            }else{
+                                                                dplusvalue.setError("Grade Value should be entered! ");
+                                                            }
+
+                                                        }else{
+                                                            cminvalue.setError("Grade Value should be entered! ");
+                                                        }
+
+                                                    }else{
+                                                        cvalue.setError("Grade Value should be entered! ");
+                                                    }
+
+                                                }else{
+                                                    cplusvalue.setError("Grade Value should be entered! ");
+                                                }
+
+                                            }else{
+                                                bminvalue.setError("Grade Value should be entered! ");
+                                            }
+
+                                        }else{
+                                            bvalue.setError("Grade Value should be entered! ");
+                                        }
+
+                                    }else{
+                                        bplusvalue.setError("Grade Value should be entered! ");
+                                    }
+
+                                }else{
+                                    aminvalue.setError("Grade Value should be entered! ");
+                                }
+
+                            }else{
+                                avalue.setError("Grade Value should be entered! ");
+                            }
+
+                        }else{
+                            aplusvalue.setError("Grade Value should be entered! ");
+                        }
 
                     }
                 });
